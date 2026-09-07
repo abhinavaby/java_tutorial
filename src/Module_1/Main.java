58da5b7e3828a1d7a0e45ed34bfd22f59cb3b3f1
abhinavaby <abhinavaby07@gmail.com>
2026-09-07 07:31:48 +0530

    tutorial 1

---
+++ src/Module_1/Main.java
@@ -0,0 +1,21 @@
+package Module_1;
+
+class Car {
+    private String model;
+
+    public Car(String model) {
+        this.model = model;
+    }
+
+    public void showModel() {
+        System.out.println("Model: " + model);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car("Tesla Model 3");
+
+        myCar.showModel();
+    }
+}
